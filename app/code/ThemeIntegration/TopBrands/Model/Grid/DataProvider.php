701df2e869bc0eebb008c7632d341d17c269d9fe
AlenEglobe <alen.george@eglobeits.com>
2024-02-20 19:13:50 +0530

    refactor: for image

--- app/code/ThemeIntegration/TopBrands/Model/Grid/DataProvider.php
+++ app/code/ThemeIntegration/TopBrands/Model/Grid/DataProvider.php
@@ -23,10 +23,22 @@ class DataProvider extends AbstractDataProvider
 
     public function getData()
     {
-        $items = $this->collection->getItems();
-        foreach ($items as $model) {
-            $this->loadedData[$model->getId()] = $model->getData();
+        if (isset($this->loadedData)) {
+            return $this->loadedData;
         }
+        $items = $this->collection->getItems(); // your saved table data's collection model 
+        foreach ($items as $brand) {
+            $brandData = $brand->getData();
+
+            if ($brand->getImage()) {  // check images saved path from the database.
+                $image_temp[0]['name'] = $brand->getImage();
+                $image_temp[0]['url'] = $brand->getImage();
+                $brandData['Image'] = $image_temp;
+            }
+
+            $this->loadedData[$brand->getArticleId()] = $brandData;
+        }
+
         return $this->loadedData;
     }
 }
