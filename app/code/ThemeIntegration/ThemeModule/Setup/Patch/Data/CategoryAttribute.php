d0cf012c520410d682ad3d4de19d75fc8b41bd7a
AlenEglobe <alen.george@eglobeits.com>
2024-03-06 16:04:17 +0530

    refactor:  use phpcs to check coding convention and fix the standard of categoryAttribute folder .

--- app/code/ThemeIntegration/ThemeModule/Setup/Patch/Data/CategoryAttribute.php
+++ app/code/ThemeIntegration/ThemeModule/Setup/Patch/Data/CategoryAttribute.php
@@ -2,6 +2,8 @@
 
 namespace ThemeIntegration\ThemeModule\Setup\Patch\Data;
 
+use Magento\Catalog\Model\Category;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
 use Magento\Eav\Setup\EavSetup;
 use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
@@ -9,47 +11,70 @@ use Magento\Framework\Setup\Patch\DataPatchInterface;
 
 class CategoryAttribute implements DataPatchInterface
 {
-    private $_moduleDataSetup;
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
 
-    private $_eavSetupFactory;
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
 
+    /**
+     * CategoryAttribute constructor.
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
     public function __construct(
         ModuleDataSetupInterface $moduleDataSetup,
         EavSetupFactory $eavSetupFactory
     ) {
-        $this->_moduleDataSetup = $moduleDataSetup;
-        $this->_eavSetupFactory = $eavSetupFactory;
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
     }
 
+    /**
+     * Apply patch
+     */
     public function apply()
     {
-        /** @var EavSetup $eavSetup */
-        $eavSetup = $this->_eavSetupFactory->create(['setup' => $this->_moduleDataSetup]);
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
 
-        $eavSetup->addAttribute(\Magento\Catalog\Model\Category::ENTITY, 'category_attribute1', [
-            'type' => 'text',
-            'label' => 'Top Category',
-            'input' => 'boolean',
-
-
-            'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
-
-            'user_defined' => false,
-            'default' => 0,
-            'group' => 'General Information',
-        ]);
+        $eavSetup->addAttribute(
+            Category::ENTITY,
+            'category_attribute1',
+            [
+                'type' => 'text',
+                'label' => 'Top Category',
+                'input' => 'boolean',
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'user_defined' => false,
+                'default' => 0,
+                'group' => 'General Information',
+            ]
+        );
     }
 
+    /**
+     * @inheritdoc
+     */
     public static function getDependencies()
     {
         return [];
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getAliases()
     {
         return [];
     }
 
+    /**
+     * @inheritdoc
+     */
     public static function getVersion()
     {
         return '1.0.0';
